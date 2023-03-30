a78f1ebe0389ca92881fd2dbbe6f243d7b21be62
TranHuy2k2 <giahuytran2002@gmail.com>
2023-03-30 08:35:54 +0700

    fixed error mapper in category

--- src/main/java/com/ctu/Library/Category/DTO/CategoryDTO.java
+++ src/main/java/com/ctu/Library/Category/DTO/CategoryDTO.java
@@ -1,5 +1,14 @@
 package com.ctu.Library.Category.DTO;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+
+@Builder
+@Data
+@RequiredArgsConstructor
+@AllArgsConstructor
 public class CategoryDTO {
     private Long id;
     private String name;
