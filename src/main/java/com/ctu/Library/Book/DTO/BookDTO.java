cdd282897d54e88b47d7fab2d6a67f88e2f85eac
Hoang Duy <duyhoang3d2y@gmail.com>
2023-03-31 12:25:02 +0700

    update

---
+++ src/main/java/com/ctu/Library/Book/DTO/BookDTO.java
@@ -0,0 +1,19 @@
+package com.ctu.Library.Book.DTO;
+
+import com.ctu.Library.Category.Category;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+
+@Builder
+@Data
+@RequiredArgsConstructor
+@AllArgsConstructor
+public class BookDTO {
+    private Long id;
+    private String ten;
+    private String namXB;
+    private String tacGia;
+    private Category categoryId;
+}
